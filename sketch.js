f0a3f8e872e7c14848a17767a356a2eb09cbe668
inuthesis <92132135+inuthesis@users.noreply.github.com>
2021-10-07 20:55:47 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200, 200, 10, 10)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+
+  if(keyIsDown(RIGHT_ARROW)) {
+    box.position.x = box.position.x + 5;
+
+    
+  }
+  if(keyIsDown(LEFT_ARROW)) {
+    box.position.x = box.position.x - 5;
+
+    
+  }
+
+  
+}
+
+
+
+
+
